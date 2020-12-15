9448b86216029c4fe0c31a1642825ab7c2252dcb
Maximilian Hänsel <mh@maxhaensel.de>
2020-12-15 23:28:28 +0100

    comment main.go

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ import (
 )
 
 func main() {
+	// Fetch root-schema
 	s := schema.GetRootSchema()
+	// parse schema and validate
 	schema := graphql.MustParseSchema(*s, &resolver.Resolver{})
+	// create basic http-handler
 	http.Handle("/query", &relay.Handler{Schema: schema})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
